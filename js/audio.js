8a7de0eef16824cc836c662e9df1e49cb0e9dfe2
macsbook <maks.naumovich@gmail.com>
2024-02-01 20:43:08 +0100

    add .gitignore

--- js/audio.js
+++ js/audio.js
@@ -1,3 +1,5 @@
+import playList from "./playList.js";
+
 const audio = document.querySelector("audio");
 
 export const playAudioButton = document.querySelector(".play");
@@ -8,28 +10,6 @@ const audioList = document.querySelector(".play-list");
 
 let isPlay = false;
 let playNum = 0;
-
-const playList = [
-    {
-        title: 'Aqua Caelestis',
-        src: '../assets/sounds/Aqua Caelestis.mp3',
-        duration: '00:58'
-    }, {
-        title: 'River Flows In You',
-        src: '../assets/sounds/River Flows In You.mp3',
-        duration: '03:50'
-    },
-    {
-        title: 'Summer Wind',
-        src: '../assets/sounds/Summer Wind.mp3',
-        duration: '05:05'
-    },
-    {
-        title: 'Ennio Morricone',
-        src: '../assets/sounds/Ennio Morricone.mp3',
-        duration: '05:03'
-    }
-]
 
 export function createAudioList() {
     for (let i = 0; i < playList.length; i++) {
